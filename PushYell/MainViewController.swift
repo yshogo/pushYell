28637c737f6ca60b601ca0cbd9c8a43f37dbc53b
shogo.yamada <superman199323@gmail.com>
2017-05-14 17:04:41 +0900

    initialize commit

--- PushYell/MainViewController.swift
+++ PushYell/MainViewController.swift
@@ -14,19 +14,31 @@ class MainViewController: ViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // 通知を使用可能にする設定
         let center = UNUserNotificationCenter.current()
         center.requestAuthorization(options: [.alert, .sound, .badge]) {
             (granted, error) in
             // エラー処理
-            
-            print("エラーが発生しました")
         }
         
+        // 通知自体の設定
         let content = UNMutableNotificationContent()
-        content.title = NSString.localizedUserNotificationString(forKey: "通知テスト", arguments: nil)
-        content.body = NSString.localizedUserNotificationString(forKey:  "できましたか", arguments: nil)
+        
+        content.title = NSString.localizedUserNotificationString(forKey: "通知タイトル", arguments: nil)
+        content.body = NSString.localizedUserNotificationString(forKey: "通知するメッセージ", arguments: nil)
         content.sound = UNNotificationSound.default()
         
+        // アプリを起動して5秒後に通知を送る
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "my-notification", content: content, trigger: trigger)
+        
+        // 通知を登録
+        center.add(request) { (error : Error?) in
+            if error != nil {
+                // エラー処理
+            }
+        }
     }
     
     override func didReceiveMemoryWarning() {
