55b5f01c91fed7a21f30a909d5ae61144771ff2d
shogo.yamada <superman199323@gmail.com>
2017-05-22 12:38:16 +0900

    add layout

---
+++ PushYell/PushPrefViewController.swift
@@ -0,0 +1,24 @@
+//
+//  PushPrefViewController.swift
+//  PushYell
+//
+//  Created by ShogoYamada on 2017/05/22.
+//  Copyright © 2017年 山田尚吾. All rights reserved.
+//
+
+import UIKit
+
+class PushPrefViewController: MainViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
