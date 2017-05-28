049e43d84650240090d0a276ca70ed2b44f09fe5
shogo.yamada <superman199323@gmail.com>
2017-05-28 18:53:19 +0900

    add navigationcontroller

---
+++ PushYell/HomeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  HomeViewController.swift
+//  PushYell
+//
+//  Created by ShogoYamada on 2017/05/28.
+//  Copyright © 2017年 山田尚吾. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: MainViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
